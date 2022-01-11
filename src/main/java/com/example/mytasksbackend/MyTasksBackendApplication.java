8f4079c56f98316abea96e3effd3e36873dd0db4
Wilson Brandão <wilsonbrandaocosta@gmail.com>
2022-01-11 20:52:43 -0300

    Initial Commit

---
+++ src/main/java/com/example/mytasksbackend/MyTasksBackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.mytasksbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyTasksBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyTasksBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/mytasksbackend/MyTasksBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.mytasksbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyTasksBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
